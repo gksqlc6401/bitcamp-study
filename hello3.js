9dfbb820ef69efdd4ee2e6ef67cb57272fbace0d
gksqlc6401 <gksqlc6401@naver.com>
2021-06-09 15:40:36 +0900

    앞뒤

--- hello3.js
+++ hello3.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 console.log("a...뒤");
 console.log("Hello, world!2");
 console.log("a...뒤");
-=======
 console.log("a...앞")
 console.log("Hello, world!2"); 
 console.log("a...앞")
->>>>>>> efdc377bdbee6d26eed9670d9e5b4a2382600627
