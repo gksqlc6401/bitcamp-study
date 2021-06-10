2a6a52bd2c10766d3310f20c83ec04773140e0ad
gksqlc6401 <gksqlc6401@naver.com>
2021-06-10 10:13:22 +0900

    .

--- hello2.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello, world!2"); 
-console.log("나는야 B 개발자");
-var name = "홍길동";
-console.log("이름: "+ studentName);
-console.log("오호라,, 나는야 A 개발자");
-console.log("Hello, world!2");  
-console.log("나는야 B 개발자");
-console.log("축하해1");
-
--- hello3.js
+++
@@ -1,6 +0,0 @@
-console.log("a...뒤");
-console.log("Hello, world!2");
-console.log("a...뒤");
-console.log("a...앞")
-console.log("Hello, world!2"); 
-console.log("a...앞")
